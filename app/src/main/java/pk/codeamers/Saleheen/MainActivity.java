aa5fef9866d54399581736de8ab19bdd220997f9
Usman <1998usman@gmail.com>
2021-11-10 21:45:48 +0500

    Splash Screen Added

--- app/src/main/java/pk/codeamers/Saleheen/MainActivity.java
+++ app/src/main/java/pk/codeamers/Saleheen/MainActivity.java
@@ -8,8 +8,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        setTheme(R.style.splashScreenTheme);
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
     }
 }
