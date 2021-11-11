ec038831f075c3898d51c4770ca45289199d15d5
Usman <1998usman@gmail.com>
2021-11-11 15:44:25 +0500

    Adding new activities..

---
+++ app/src/main/java/pk/codeamers/Saleheen/QiblaDirection.java
@@ -0,0 +1,14 @@
+package pk.codeamers.Saleheen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class QiblaDirection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qibla_direction);
+    }
+}
