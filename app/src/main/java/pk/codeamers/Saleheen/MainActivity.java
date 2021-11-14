76323f874533f31ea64b1244a9eeecae5eaaad14
Usman <1998usman@gmail.com>
2021-11-14 14:37:52 +0500

    Added Color to the Ui

--- app/src/main/java/pk/codeamers/Saleheen/MainActivity.java
+++ app/src/main/java/pk/codeamers/Saleheen/MainActivity.java
@@ -18,27 +18,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-         super.onCreateOptionsMenu(menu);
-        MenuInflater menuInflater = getMenuInflater();
-        menuInflater.inflate(R.menu.optionmenu, menu);
-         return  true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-         super.onOptionsItemSelected(item);
-         switch (item.getItemId()){
-             case R.id.home:
-                 Intent i = new Intent(MainActivity.this , QiblaDirection.class);
-                 startActivity(i);
-             case R.id.about:
-                 Intent in = new Intent(MainActivity.this , QiblaDirection.class);
-                 startActivity(in);
-         }
-    return true;
-    }
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//         super.onCreateOptionsMenu(menu);
+//        MenuInflater menuInflater = getMenuInflater();
+//        menuInflater.inflate(R.menu.optionmenu, menu);
+//         return  true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+//         super.onOptionsItemSelected(item);
+//         switch (item.getItemId()){
+//             case R.id.home:
+//                 Intent i = new Intent(MainActivity.this , QiblaDirection.class);
+//                 startActivity(i);
+//             case R.id.about:
+//                 Intent in = new Intent(MainActivity.this , QiblaDirection.class);
+//                 startActivity(in);
+//         }
+//    return true;
+//    }
 
     public void showQiblaDirection(View v) {
         Intent i = new Intent(MainActivity.this , QiblaDirection.class);
