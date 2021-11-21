4779627143189d86ee741143c1614f9e4b5a06e6
Usman <1998usman@gmail.com>
2021-11-21 15:11:21 +0500

    Salah Tracker Added 50%

--- app/src/main/java/pk/codeamers/Saleheen/MainScreen.java
+++ app/src/main/java/pk/codeamers/Saleheen/MainScreen.java
@@ -24,15 +24,23 @@ public class MainScreen extends Fragment {
         View root = inflater.inflate(R.layout.fragment_main_screen, container, false);
 
         // Fragment To Activity
+        // Quran
         LinearLayout btn = root.findViewById(R.id.btnQuran);
         btn.setOnClickListener(v -> {
             Intent i = new Intent(getActivity(), Quran.class);
             startActivity(i);
         });
+        //Google Map
         LinearLayout btn4 = root.findViewById(R.id.gotoMapLayout);
         btn4.setOnClickListener( v -> {
             Intent in = new Intent(getActivity(), NearestMosque.class);
             startActivity(in);
+        });
+        //SalahTracker
+        LinearLayout btn5 = root.findViewById(R.id.salahTracker);
+        btn5.setOnClickListener( v -> {
+            Intent i = new Intent(getActivity(), SalahTracker.class);
+            startActivity(i);
         });
 
         return root;
